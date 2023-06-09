cb2cc6804905d5274ec26f2585cc9c3b007d2370
Artem M <artemmarin91@mail.ru>
2023-06-09 12:23:25 +0500

    added ChatBox component

---
+++ src/components/ChatBox/ChatBox.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import s from './ChatBox.module.scss';
+import { useAppSelector, useAuth } from '../../hooks';
+import SendMessageForm from '../SendMessageForm';
+import MessagesBox from '../MessagesBox';
+
+const ChatBox = () => {
+  const currentChatId = useAppSelector((state) => state.chats.currentChatId);
+  const currentChat = useAppSelector((state) => state.chats.chats)
+    .find((el) => el.id === currentChatId);
+  const headerString = currentChat?.name ? currentChat.name : currentChatId;
+
+  const { logOut } = useAuth();
+
+  return (
+    <div className={s.page}>
+      <div className={s.header}>
+        {headerString && <span>{headerString}</span>}
+        {!headerString && <span>Выберите чат из списка</span>}
+        <button onClick={logOut} type="button" className={s.headerButton}>
+          Выйти
+        </button>
+      </div>
+      <MessagesBox />
+      <SendMessageForm />
+    </div>
+  );
+};
+
+export default ChatBox;
