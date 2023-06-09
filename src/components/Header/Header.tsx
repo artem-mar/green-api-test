e6e847b13b4343dd37053dec8d7b87dec4c1848c
Artem M <artemmarin91@mail.ru>
2023-06-09 12:52:32 +0500

    small corrections

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,16 +2,12 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './Header.module.scss';
 
-const Header = () => {
-  const navigate = useNavigate();
-
-  return (
-    <div className={styles.nav}>
-      <div className={styles.logo}>
-        <span className={styles.header}>not WhatsApp</span>
-      </div>
+const Header = () => (
+  <div className={styles.nav}>
+    <div className={styles.logo}>
+      <span className={styles.header}>not WhatsApp</span>
     </div>
-  );
-};
+  </div>
+);
 
 export default Header;
