45f7cfcbd660cc1d9c34acc72a983d287a04cb07
Artem M <artemmarin91@mail.ru>
2023-06-09 12:20:45 +0500

    added new routes

--- src/routes.ts
+++ src/routes.ts
@@ -3,8 +3,12 @@ const host = 'https://api.green-api.com';
 const routes = {
   getStateInstance: (id: string, token: string) => `${host}/waInstance${id}/getStateInstance/${token}`,
   receiveNotification: (id: string, token: string) => `${host}/waInstance${id}/receiveNotification/${token}`,
-  deleteNotification: (instanceId: string, token: string, notificationId: number) => `${host}/waInstance${instanceId}/deleteNotification/${token}/${notificationId}`,
+  deleteNotification: (instanceId: string, token: string, notificationId: number) => (
+    `${host}/waInstance${instanceId}/deleteNotification/${token}/${notificationId}`
+  ),
   getChats: (id: string, token: string) => `${host}/waInstance${id}/getChats/${token}`,
+  sendMessage: (id: string, token: string) => `${host}/waInstance${id}/sendMessage/${token}`,
+  getChatHistory: (id: string, token: string) => `${host}/waInstance${id}/getChatHistory/${token}`,
 };
 
 export default routes;
