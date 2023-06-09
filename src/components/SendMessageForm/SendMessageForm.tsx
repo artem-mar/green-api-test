48d66b7020e8f3094fa6f5b6213e47393e71c7ab
Artem M <artemmarin91@mail.ru>
2023-06-09 12:24:45 +0500

    added SendMessageForm component

---
+++ src/components/SendMessageForm/SendMessageForm.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import axios from 'axios';
+import s from './SendMessageForm.module.scss';
+import { useAppSelector, useAuth } from '../../hooks';
+import routes from '../../routes';
+
+type Input = {
+  message: string;
+};
+
+const SendMessageForm = () => {
+  const currentChatId = useAppSelector((state) => state.chats.currentChatId);
+  const { apiToken, instanceId } = useAuth();
+  const {
+    resetField,
+    handleSubmit,
+    register,
+    formState: { isSubmitting },
+
+  } = useForm<Input>();
+
+  const onSubmit: SubmitHandler<Input> = async ({ message }) => {
+    const trimed = message.trim();
+    if (trimed.length > 0) {
+      try {
+        await axios.post(routes.sendMessage(instanceId, apiToken), {
+          chatId: currentChatId,
+          message,
+        });
+
+        resetField('message', { keepTouched: false });
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className={s.form} action="submit">
+      <input
+        disabled={isSubmitting || !currentChatId}
+        autoComplete="off"
+        className={s.input}
+        placeholder={currentChatId ? 'Введите сообщение...' : 'Выберите чат'}
+        {...register('message')}
+      />
+
+      <button disabled={isSubmitting || !currentChatId} className={s.btn} type="submit">
+        &#10148;
+      </button>
+    </form>
+  );
+};
+
+export default SendMessageForm;
